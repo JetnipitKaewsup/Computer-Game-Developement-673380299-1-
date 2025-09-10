f21336a823640228cca4cb50b048816df422a29e
JetnipitKaewsup <Jetnipit.Kaewsup@gmail.com>
2025-09-10 18:34:51 +0700

    import characters

--- script/player.gd
+++ script/player.gd
@@ -62,10 +62,13 @@ func _physics_process(delta):
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
 		$Pivot.basis = Basis.looking_at(direction)
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
-
+	
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
@@ -73,7 +76,7 @@ func _physics_process(delta):
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
-
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
 	
 # And this function at the bottom.
 func die():
